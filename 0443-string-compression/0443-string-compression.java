e0de3def28ac5af0cf2f010aad79574f5ee6cd3a
Aryan Saini <aryansaini7213@gmail.com>
2026-07-23 09:30:18 +0530

    Time: 1 ms (99.89%), Space: 45.5 MB (48.71%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,36 +1,24 @@
 class Solution {
     public int compress(char[] chars) {
-
-        int i = 0;
         int index = 0;
+        int i = 0;
+        while(i<chars.length){
 
-        while (i < chars.length) {
-
-            int j = i;
-
-            // Find the end of the current group
-            while (j < chars.length && chars[j] == chars[i]) {
+            int j =i;
+            while(j<chars.length && chars[j]==chars[i]){
                 j++;
             }
-
-            int count = j - i;
-
-            // Write the character
+            int count = j-i;
             chars[index++] = chars[i];
-
-            // Write the count only if greater than 1
             if (count > 1) {
                 String s = String.valueOf(count);
-
                 for (char c : s.toCharArray()) {
                     chars[index++] = c;
                 }
-            }
-
-            // Move to the next group
+            }        
             i = j;
-        }
 
+        }
         return index;
     }
 }
